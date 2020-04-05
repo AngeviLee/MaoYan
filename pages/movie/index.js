d135ce498da22be758d1d11de7b266f3f79db241
AngeviLee <568178364@qq.com>
2020-04-05 22:59:20 +0800

    猫眼电影-热映、待映切换样式和对应数据

--- pages/movie/index.js
+++ pages/movie/index.js
@@ -1,3 +1,11 @@
 Page({
-  
+  data: {
+      tabIndex: 'hots'
+  },
+  switch (ev) {
+      console.log(ev)
+      this.setData({
+          tabIndex: ev.target.dataset.tabIndex
+      })
+  }
 });
